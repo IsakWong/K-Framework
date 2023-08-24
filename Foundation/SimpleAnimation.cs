291197d945a61f31e969311144c88c7a2a6be5f9
Isaac Wong <IsakWong@outlook.com>
2023-08-24 11:00:28 +0800

    核心模块代码提交

---
+++ Foundation/SimpleAnimation.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Animations;
+using UnityEngine.Playables;
+
+[RequireComponent(typeof(Animator))]
+public class SimpleAnimation : MonoBehaviour
+{
+    public bool mPlayOnAwake = true;
+    public List<AnimationClip> clipsToPlay = new();
+    public bool mLoop = false;
+    private PlayableGraph playableGraph;
+
+    void Start()
+    {
+    }
+
+    void OnDisable()
+    {
+    }
+}
---
+++ UI/UIAnimAnimation.cs
@@ -0,0 +1,7 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using DG.Tweening;
+using K1.Gameplay;
+using TMPro;
+using UnityEngine;
